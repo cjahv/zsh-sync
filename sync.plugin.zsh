e94b3751f06a05ce414c2c175237e55508bcfc0a
jahv <me@cjahv.com>
2022-01-18 12:33:02 +0800

    修复/

--- sync.plugin.zsh
+++ sync.plugin.zsh
@@ -63,5 +63,5 @@ function syncl() {
     [ -f $HOME/.config/sync/exclude.txt ] && exclude_from="--exclude-from=$HOME/.config/sync/exclude.txt"
     [ -f $dir.syncignore ] && exclude_from="--exclude-from=$dir.syncignore $exclude_from"
     dir="${dir// /\\ }/"
-    ssh $USER@$SYNC_HOST "ls $dir | sed 's:^:$dir:'| sed 's/\/\///'| sed 's/ /\\\\ /'"
+    ssh $USER@$SYNC_HOST "ls $dir | sed 's:^:$dir:'| sed 's/\/\//\//'| sed 's/ /\\\\ /'"
 }
